0710ad2f4506d49ee913f0549528e531d76469ec
Brut <brutius666@gmail.com>
2017-07-27 15:25:50 +0300

    mob

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 $( document ).ready(function() {
     $('.button').on('click', function (el) {
-        var parent = this.closest('.message');
-        if($(this).hasClass('next')){
-            $(parent).removeClass('active').next().addClass('active');
-        }else if(($(this).hasClass('splash'))){
-			var w = $(window).width();
-			$('.back-full').hide(1000);
-			$('.back').show(1000);
-			$(this).closest('.message').removeClass('active').next().addClass('active');
+		var parent = this.closest('.message');
+		var w = $(window).width();
+		var num = parent.getAttribute('data-slide');
+		if($(this).hasClass('next')){
+			$(parent).removeClass('active').next().addClass('active');
+			if(w < 460){
+				if(num > 2 && num < 7){
+					$('.back-full').addClass('back-2');
+				}else if(num >=7){
+					$('.back-full').addClass('back-3');
+				}
+			}else{
+				$('.back-full').addClass('fullback');
+			}
 		}else{
-           $(parent).removeClass('active').prev().addClass('active'); 
-        }
-    })
+			$(parent).removeClass('active').prev().addClass('active'); 
+		} 
+	})
 });
